cf252531a2863a4771af644a23c115ce1d3c96ac
NatalNW7 <natanaelweslley1@gmail.com>
2024-08-17 16:51:34 -0300

    feat: add UpdateAt in LinkResponse

--- core/schemas/link.go
+++ core/schemas/link.go
@@ -15,6 +15,7 @@ type Link struct {
 type LinkReponse struct {
 	ID uint  `json:"id"`
 	CreateAt time.Time `json:"createAt"`
+	UpdateAt time.Time `json:"updateAt"`
 	DeleteAt time.Time `json:"deleteAt,omitempty"`
 	LinkId string `json:"linkId"`
 	RedirectUrl string `json:"redirectUrl"`
