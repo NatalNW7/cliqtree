007fa935a74ec18908e880ef7c2dc303f31e99b9
NatalNW7 <natanaelweslley1@gmail.com>
2024-08-23 14:26:54 -0300

    feat: add handler_test.go

---
+++ test/handler_test.go
@@ -0,0 +1,113 @@
+package test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/NatalNW7/link.in/pkg/config"
+	"github.com/NatalNW7/link.in/pkg/server/handler"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	linkID string
+)
+
+func initConfig() error {
+	err := config.Init()
+	if err != nil {
+		return fmt.Errorf("Error to initialize project configuration: %v", err)
+	}
+	handler.Init()
+	return nil
+}
+
+func createGinContext(recorder *httptest.ResponseRecorder) *gin.Context {
+	initConfig()
+	gin.SetMode(gin.TestMode)
+
+	ctx, _ := gin.CreateTestContext(recorder)
+	ctx.Request = &http.Request{
+		Header: make(http.Header),
+		URL: &url.URL{},
+	}
+
+	return ctx
+}
+
+func MakeGet(ctx *gin.Context, params gin.Params){
+	ctx.Request.Method = "GET"
+	ctx.Params = params
+}
+
+func MakePost(ctx *gin.Context, body interface{}){
+	ctx.Request.Method = "POST"
+	jsonBody, _ := json.Marshal(body)
+	ctx.Request.Body = io.NopCloser(bytes.NewBuffer(jsonBody))
+	ctx.Request.Header.Set("Content-Type", "application/json")
+}
+
+func TestCreateLink(t *testing.T){
+	recorder := httptest.NewRecorder()
+	ctx := createGinContext(recorder)
+	body := map[string]string{
+		"url": "www.github.com",
+	}
+
+	MakePost(ctx, body)
+	handler.CreateShortLink(ctx)
+
+	if recorder.Code != http.StatusCreated {
+		t.Errorf("expect %d and got: %d", http.StatusCreated, recorder.Code)
+	}
+
+	var response map[string]interface{}
+	err := json.Unmarshal(recorder.Body.Bytes(), &response)
+	if err != nil {
+		t.Errorf("Not possible to unmarshal body response: %s", err.Error())
+	}
+	data := response["data"].(map[string]interface{})
+	linkID = data["linkId"].(string)
+}
+
+func TestFindLink(t *testing.T){
+	recorder := httptest.NewRecorder()
+	ctx := createGinContext(recorder)
+	params := []gin.Param{
+		{
+			Key: "linkId",
+			Value: linkID,
+		},
+	}
+
+	MakeGet(ctx, params)
+	handler.FindShortLinkByLinkId(ctx)
+
+	if recorder.Code != http.StatusOK {
+		t.Errorf("expect %d and got: %d",http.StatusOK , recorder.Code)
+	}
+}
+
+func TestMethodNotAllowed(t *testing.T){
+	recorder := httptest.NewRecorder()
+	ctx := createGinContext(recorder)
+	ctx.Request.Method = http.MethodDelete
+	ctx.Params = []gin.Param{
+		{
+			Key: "linkId",
+			Value: "testId",
+		},
+	}
+
+	handler.FindShortLinkByLinkId(ctx)
+
+	if recorder.Code != http.StatusMethodNotAllowed {
+		t.Errorf("expect %d and got: %d", http.StatusMethodNotAllowed, recorder.Code)
+	}
+}
